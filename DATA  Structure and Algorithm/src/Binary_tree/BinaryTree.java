38161d4f60a3f7b7fdea84f6eba7cb23cacf9215
akhilsingh2003 <akhilsinghrajpoot1@gmail.com>
2022-03-31 21:21:01 +0530

    updated

--- DATA  Structure and Algorithm/src/Binary_tree/BinaryTree.java
+++ DATA  Structure and Algorithm/src/Binary_tree/BinaryTree.java
@@ -1,10 +1,13 @@
 package Binary_tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
-    private TreeNode root;
-    private class TreeNode{
-        private TreeNode left,right;
-        private int data;
+    public TreeNode root;
+     public class TreeNode{
+        public TreeNode left,right;
+        public int data;
         public TreeNode(int data){
             this.data=data;
         }
@@ -32,10 +35,51 @@ public class BinaryTree {
         PreOrder(root.right);
     }
 
+    public void Inorder(TreeNode root){
+        if(root==null) return;
+        Inorder(root.left);
+        System.out.print(root.data + " ");
+        Inorder(root.right);
+    }
+    public void PostOrder(TreeNode root){
+        if(root== null) return;
+        PostOrder(root.left);
+        PostOrder(root.right);
+        System.out.print(root.data+" ");
+    }
+    public void LevelOrder(TreeNode root){
+         if(root==null) return;
+        Queue<TreeNode> queue =new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode temp=queue.poll();
+            System.out.print(temp.data+" ");
+            if(temp.left!=null){
+                queue.offer(temp.left);
+            }
+            if(temp.right!=null){
+                queue.offer(temp.right);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         BinaryTree bt=new BinaryTree();
         bt.createBinaryTree();
+        System.out.println("_______________________\n");
+        System.out.println("PreOder Traversal\n");
         bt.PreOrder(bt.root);
+        System.out.println("\n_______________________\n");
+        System.out.println("Inorder Traversal\n");
+        bt.Inorder(bt.root);
+        System.out.println("\n_______________________\n");
+        System.out.println("PostOrder Traversal\n");
+        bt.PostOrder(bt.root);
+        System.out.println("\n_______________________\n");
+        System.out.println("Level Order\n");
+        bt.LevelOrder(bt.root);
+
+
     }
 }
 

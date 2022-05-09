30b039ffed702ae22e5fafa299882b89d429da78
akhilsingh2003 <akhilsinghrajpoot1@gmail.com>
2022-05-09 22:07:22 +0530

    updated

--- DATA  Structure and Algorithm/src/Binary_tree/Binary_Search_Tree.java
+++ DATA  Structure and Algorithm/src/Binary_tree/Binary_Search_Tree.java
@@ -31,6 +31,49 @@ public class Binary_Search_Tree {
         return root;
     }
 
+    public static TreeNode minimumElement(TreeNode root) {
+        if (root.left == null)
+            return root;
+        else {
+            return minimumElement(root.left);
+        }
+    }
+
+    public static TreeNode deleteNode(TreeNode root, int value) {
+        if (root == null)
+            return null;
+        if (root.data > value) {
+            root.left = deleteNode(root.left, value);
+        } else if (root.data < value) {
+            root.right = deleteNode(root.right, value);
+
+        } else {
+            // if nodeToBeDeleted have both children
+            if (root.left != null && root.right != null) {
+                TreeNode temp = root;
+                // Finding minimum element from right
+                TreeNode minNodeForRight = minimumElement(temp.right);
+                // Replacing current node with minimum node from right subtree
+                root.data = minNodeForRight.data;
+                // Deleting minimum node from right now
+                root.right = deleteNode(root.right, minNodeForRight.data);
+
+            }
+            // if nodeToBeDeleted has only left child
+            else if (root.left != null) {
+                root = root.left;
+            }
+            // if nodeToBeDeleted has only right child
+            else if (root.right != null) {
+                root = root.right;
+            }
+            // if nodeToBeDeleted do not have child (Leaf node)
+            else
+                root = null;
+        }
+        return root;
+    }
+
     public void inOrder() {
         inOrder(root);
     }
